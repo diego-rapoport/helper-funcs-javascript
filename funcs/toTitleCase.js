295efb9303546b78da24508fe81a52e2814ce1fe
Diego Rapoport <diegomrapoport@gmail.com>
2024-05-07 13:54:52 -0300

    update return toTileCase docs

--- funcs/toTitleCase.js
+++ funcs/toTitleCase.js
@@ -1,7 +1,8 @@
 /** 
  * Takes a word and turns into title case.
- * @param {string} word
- * **/
+ * @param {string} a word
+ * @returns {string} another word
+ **/
 export function toTitleCase(word) {
   return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
 }
