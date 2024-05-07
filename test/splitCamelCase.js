1f508c07f1432e7caa65e4d2cf51d031225da76b
Diego Rapoport <diegomrapoport@gmail.com>
2024-05-07 14:03:28 -0300

    fix camelCase test with comparTwoArrays

--- test/splitCamelCase.js
+++ test/splitCamelCase.js
@@ -2,12 +2,8 @@ import assert from 'node:assert'
 import {describe, it} from 'node:test'
 
 import {splitCamelCase} from '../funcs/splitCamelCase.js'
+import { compareTwoArrays } from '../funcs/compareTwoArrays.js'
 
-function compareTwoArrays(arr1, arr2) {
-  return arr1.length === arr2.length && arr1.every((item, index) => {
-    return item === arr2[index]
-  })
-}
 describe('Split camel case strings', () => {
   it('should return an array splitted from camel case string', () => {
     const arrayCompare = ['one', 'First', 'Test']
