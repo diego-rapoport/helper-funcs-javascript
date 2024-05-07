55397a385d366c85c14fd1b5f92f9a35e28f6104
Diego Rapoport <diegomrapoport@gmail.com>
2024-05-07 13:04:51 -0300

    export function as module

--- funcs/toTitleCase.js
+++ funcs/toTitleCase.js
@@ -2,8 +2,6 @@
  * Takes a word and turns into title case.
  * @param {string} word
  * **/
-function toTitleCase(word) {
+export function toTitleCase(word) {
   return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
 }
-
-module.exports = toTitleCase
