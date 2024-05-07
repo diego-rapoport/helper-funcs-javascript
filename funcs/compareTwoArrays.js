63b9736e6ad2e0dfcbb339a822e9326108b838f2
Diego Rapoport <diegomrapoport@gmail.com>
2024-05-07 14:03:08 -0300

    make compareTwoArrays another helper function

---
+++ funcs/compareTwoArrays.js
@@ -0,0 +1,11 @@
+/**
+ * @param {Array<string|number>} arr1 first array
+ * @param {Array<string|number>} arr2 second array
+ * @returns {boolean} true if items inside matches strictly
+ **/
+export function compareTwoArrays(arr1, arr2) {
+  return arr1.length === arr2.length && arr1.every((item, index) => {
+    return item === arr2[index]
+  })
+}
+
